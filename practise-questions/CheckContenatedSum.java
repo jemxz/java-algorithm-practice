3d9e4be22befac1f16d012719c6259335051172e
jemxz <32041644+jemxz@users.noreply.github.com>
2021-08-13 02:26:21 +0300

    Update CheckContenatedSum.java
    I think you wrote the answer of a different question.

--- practise-questions/CheckContenatedSum.java
+++ practise-questions/CheckContenatedSum.java
@@ -4,23 +4,27 @@ adjacent elements and returning the largest such sum. You may assume that the ar
 elements.
 */
 
-public class CheckContenatedSum {
-  public static void main(String[] args){
-    System.out.println(f(9,1));
-  }
-
-  public static int f(int a, int c){
-    int sum = 0;
-    String dig = "";
-    String numstr = String.valueOf(a);
-    for(int i=0; i<numstr.length(); i++){
-      dig = "";
-      for(int j=0; j<c; j++)
-        dig += ""+numstr.charAt(i);
-      sum += Integer.parseInt(dig);
+public class Main {
+    public static void main(String[] args){
+        int[] x = {12,3,4,5,2,3,10};
+        System.out.println(largestAdjacentSum(x));
     }
-    if(sum == a)
-      return 1;
-    return 0;
-  }
+
+    public static int largestAdjacentSum(int[] a){
+        if (a.length<1) return 0;
+        int highestCount = 0;
+        for(int i=0; i<a.length; i++){
+            if (i==a.length-1) break;
+            else {
+                int k = i + 1;
+                int currentValue = a[i];
+                int addedValue = a[k];
+                int count = currentValue + addedValue;
+                if (highestCount < count) highestCount = count;
+            }
+        }
+        return highestCount;
+
+    }
+
 }
